53a3a6e3475ae043cedbcc65372a77ae051e8cc5
dkh11 <dilip.kharel@infosys.com>
2022-02-17 18:42:50 -0500

    "made a few changes to print the states"

--- Multithreading/src/exercises/App1.java
+++ Multithreading/src/exercises/App1.java
@@ -15,7 +15,7 @@ class Runner2 implements Runnable{
 	@Override
 	public void run() {
 		for(int i=1; i<10; i++) {
-			System.out.println("Running2 "+i);			
+			System.out.println("	Running2 "+i);			
 		}
 	}
 }
@@ -31,10 +31,14 @@ public class App1 {
 		r2.run();
 
 		Thread t1 = new Thread(r1);
+		System.out.println("Created new thread t1 using Runner1. State = " + t1.getState());
 		Thread t2 = new Thread(r2);
-		
+		System.out.println("	Created new thread t2 using Runner2. State = " + t2.getState());
+
 		t1.start();
-		System.out.println("******************");		
+		System.out.println("Started thread 1. State = "+ t1.getState());
+		//System.out.println("******************");		
 		t2.start();
+		System.out.println("	Started thread 2. State = "+ t2.getState());
 	}
 }
--- Multithreading/src/exercises/App2.java
+++ Multithreading/src/exercises/App2.java
@@ -9,6 +9,7 @@ class A2Runner1 extends Thread {
 			System.out.println("Runner1 "+i);
 			try {
 				Thread.sleep(1000);
+				System.out.println("******** Sleeping Runner1. State = " + Thread.currentThread().getState());
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -20,9 +21,10 @@ class A2Runner2 extends Thread {
 	@Override
 	public void run() {
 		for(int i=0; i<10; i++) {
-			System.out.println("Runner2 "+i);
+			System.out.println("	Runner2 "+i);
 			try {
 				Thread.sleep(800);
+				System.out.println("########## Sleeping Runner2. State = " + Thread.currentThread().getState());
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -33,10 +35,15 @@ class A2Runner2 extends Thread {
 public class App2 {
 	public static void main(String[] args) {
 		Thread t1 = new A2Runner1();		
+		System.out.println("Created new thread t1 using Runner1. State = " + t1.getState());
 		Thread t2 = new A2Runner2();
+		System.out.println("	Created new thread t2 using Runner2. State = " + t2.getState());
 		
 		t1.start();
-		System.out.println("********************");
+		System.out.println("Started thread 1. State = "+ t1.getState());
+		//System.out.println("********************");
 		t2.start();
+		System.out.println("	Started thread 2. State = "+ t2.getState());
+		
 	}
 }
